9477052aa5d8a09ceabd7191d49ccc7f19088e29
AntoineNH2 <antoine.aminot@gmail.com>
2016-04-26 18:37:56 +0200

    Presque mais pb sur les formats des valeurs dans les txt...

--- app/src/main/java/com/nh2/antoine/isthismylanguage/ReadMatrixTxt2.java
+++ app/src/main/java/com/nh2/antoine/isthismylanguage/ReadMatrixTxt2.java
@@ -1,6 +1,7 @@
 package com.nh2.antoine.isthismylanguage;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.Scanner;
 import java.io.BufferedReader;
@@ -15,43 +16,47 @@ import java.util.List;
  */
 public class ReadMatrixTxt2 {
 
-    InputStream inputStream;
 
-    public ReadMatrixTxt2(InputStream inputStream){
-        this.inputStream = inputStream;
-    }
+    public int[][] read(String filename) throws FileNotFoundException {
 
-    public int read(){
-
-        BufferedReader input = new BufferedReader(new InputStreamReader(inputStream));
+        int [][] matrice;
+        //Scanner input = new Scanner(new File(filename));
+        BufferedReader input = new BufferedReader(new FileReader(filename));
 
         // pre-read in the number of rows/columns
         int rows = 0;
         int columns = 0;
 
-        try
-            while(input.readLine())    {
-        ++rows;
-        Scanner colReader = new Scanner(input.nextLine());
-        while(colReader.hasNextInt())
-        {
-            ++columns;
+        try {
+            while (input.readLine() != null) {
+                ++rows;
+            }
+
+            matrice = new int[rows][rows];
+            // read in the data
+            String line;
+            int i=0;
+            while((line = input.readLine()) !=null){
+                ++i;
+                //String[] row = line.split(",");
+                for (String retval: line.split(",")){
+                    ++columns;
+                    matrice[i][columns] = (int) retval;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-    }
-    int[][] a = new int[rows][columns];
-
-    input.close();
-
-// read in the data
-    input = new Scanner(new File("src/array.txt"));
-    for(int i = 0; i < rows; ++i)
-    {
-        for(int j = 0; j < columns; ++j)
-        {
-            if(input.hasNextInt())
-            {
-                a[i][j] = input.nextInt();
+        finally {
+            try {
+                input.close();
+            } catch (IOException e) {
+                throw new RuntimeException("Error while closing input stream: " + e);
             }
         }
+
+    return matrice;
+
     }
+
 }
