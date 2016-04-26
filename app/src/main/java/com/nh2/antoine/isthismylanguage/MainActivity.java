6a5246969cd719f138894097fd98685896e29630
AntoineFixe <antoine.aminot@gmail.com>
2016-04-26 10:55:08 +0200

    Rajout du bouton

--- app/src/main/java/com/nh2/antoine/isthismylanguage/MainActivity.java
+++ app/src/main/java/com/nh2/antoine/isthismylanguage/MainActivity.java
@@ -1,13 +1,31 @@
-package com.nh2.antoine.isthismylanguage;
+                package com.nh2.antoine.isthismylanguage;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+        import android.util.Log;
+        import android.view.View;
+        import android.widget.Button;
+        import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+        public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-}
+            private TextView TextAlea =null;
+            private Button BoutonAlea =null;
+
+
+            @Override
+            protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+
+                BoutonAlea = (Button) findViewById(R.id.BalanceBouton);
+                BoutonAlea.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // ICI l'action
+                    }
+                });
+
+
+            }
+        }   
